3ee78716c714aec951f81e5ece97689e5a0459e3
Bretton B Holmes <famunkwriter@me.com>
2019-11-10 04:06:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,7 +27,7 @@ def nyc_pigeon_organizer(data)
   data.each do |data_type, data_attributes| #|color, all the colors with the values|
     data_attributes.each do |key, names| #single color, all names with that color|
       names.each do |name|
-        pigeons_hash[name][data_type] = [key.to_str] 
+        pigeons_hash[name][data_type] = [key.to_s] 
 
       end
     end
